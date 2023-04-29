b714ddc7435f5b3c5661c703dd86b5a5ce214bf4
Flamascus <der.anton111@gmail.com>
2023-04-29 17:29:34 +0200

    * folder-picking * cicd pipeline

--- MP - Music Player/Services/CoverRetriever.cs
+++ MP - Music Player/Services/CoverRetriever.cs
@@ -1,6 +1,5 @@
 ﻿namespace MP_Music_Player.Services; 
 
-public partial class CoverRetriever {
-
+public partial class CoverRetriever {  
   public static partial byte[]? GetCover(string filePath);
 }
